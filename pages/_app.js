fee0ed062be2702cf58ba7e903dbef8acce3711a
rafedweb <faeldesenho@yahoo.com.br>
2021-02-19 04:01:30 +0000

    Initial commit

---
+++ pages/_app.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import theme from '../theme'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Menu from '../src/components/commons/Menu';
+
+export default function Home() {
+  return (
+          <div>
+            <Menu />          
+         </div>
+     )
+}
