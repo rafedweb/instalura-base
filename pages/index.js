a5791253eb3b8460f05f085755d9a6453ba6fe1c
Rafael <webdeveloperrm@gmail.com>
2021-02-20 00:45:37 -0300

    aula 05

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import Footer from '../src/components/commons/Footer';
 import Menu from '../src/components/commons/Menu';
 
 export default function Home() {
   return (
-          <div>
-            <Menu />          
+    <div style={{
+      flex: '1',
+      display: 'flex',
+      flexWrap: 'wrap',
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+    }}>
+            <Menu /> 
+            <Footer />         
          </div>
      )
 }
